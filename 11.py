140e710559f3b9db1c0438536f3b21b10fcfa97f
cktang88 <arctangent20@yahoo.com>
2018-12-10 23:32:13 -0600

    11 - didn't need to finish running pt 2 lol

---
+++ 11.py
@@ -0,0 +1,45 @@
+import math
+
+serial = 9435 # serial
+
+board = [[0 for i in range(300)] for j in range(300)]
+
+def hun(n):
+    if n < 100:
+        return 0
+    return int(str(n)[-3:-2]) # get hundreds dig
+
+def calc(x,y):
+    id = 10 + x
+    power = id * y
+
+    power += serial
+
+    power *= id
+    return hun(power) - 5
+
+
+for y in range(300):
+    for x in range(300):
+        board[y][x] = calc(x,y)
+
+print board
+
+m = 0
+a,b,s = 0,0,0
+
+
+for size in range(1, 300):
+    for y in range(300-size):
+        #print y
+        for x in range(300-size):
+            #for row in board[y:y+3]:
+                #print row[x:x+3]
+            tmp = sum([sum(row[x:x+size]) for row in board[y:y+size]])
+            if tmp > m:
+                m = tmp
+                a,b,s = x,y,size
+                print a,b,m,s
+
+print m, a,b
+        
